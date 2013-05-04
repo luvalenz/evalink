f7da2156e471cc1566c707cc47e1b6bb4b79c807
Lucas <lucas@lucas-pc.(none)>
2013-05-04 17:47:41 -0400

    aplicacion rails basica

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Evalink::Application.config.secret_token = '874055e26ead1a6dbb7f766776f7a8eba3685593c786a14107a82aad64cf987fa5271097fbe5c0af06a6aac0831f1310aa5a4761af51a06a2fc6a9ef31ec768e'
